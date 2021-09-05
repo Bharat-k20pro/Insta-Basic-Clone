a2fad46f8754ce492f4069e9520740406249cc2c
Bharat <jainb0206@gmail.com>
2021-09-05 13:35:50 +0530

    security commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,22 @@ import firebase from "firebase";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDiW0u7qaA3b_de0latCXx9OcoSidBF5l4",
+//   authDomain: "instagram-clone-3840a.firebaseapp.com",
+//   projectId: "instagram-clone-3840a",
+//   storageBucket: "instagram-clone-3840a.appspot.com",
+//   messagingSenderId: "254259300994",
+//   appId: "1:254259300994:web:23679228def993ed2a429b",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDiW0u7qaA3b_de0latCXx9OcoSidBF5l4",
-  authDomain: "instagram-clone-3840a.firebaseapp.com",
-  projectId: "instagram-clone-3840a",
-  storageBucket: "instagram-clone-3840a.appspot.com",
-  messagingSenderId: "254259300994",
-  appId: "1:254259300994:web:23679228def993ed2a429b",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_APPDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 // Initialize Firebase
